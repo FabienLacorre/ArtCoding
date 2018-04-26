b80413a16d06f09654e70945612f82ccd9de74d3
FabienLacorre <fab.lacorre@gmail.com>
2018-04-26 11:55:07 +0200

    remove comment

--- P5JS-FirstTry/main.js
+++ P5JS-FirstTry/main.js
@@ -1,27 +1,15 @@
+var a = 40;
+var bool = false;
+var r = 10;
+var g = 75;
+var b = 120;
+var t = 10;
+
 function setup() {
   createCanvas(630, 630);
   frameRate(60);
 }
 
-var a = 40;
-
-function RandomCircles(xSize, ySize, nb){
-    for (var i = 0; i < nb; i++) {
-        var r = random(250);
-        var g = random(250);
-        var b = random(250);
-        var a = random(250);
-        var x = random(xSize);
-        var y = random(ySize);
-        fill(r, g, b, a);
-        ellipse(x, y, 80, 80);
-    }
-}
-
-var r = 10;
-var g = 75;
-var b = 120;
-var t = 10;
 function createCircleCanvas(xSize, ySize, cSize, variableSize){
     var nbCircleX = xSize / cSize;
     var nbCircleY = ySize / cSize;
@@ -45,26 +33,15 @@ function createCircleCanvas(xSize, ySize, cSize, variableSize){
     }
 }
 
-var bool = false;
-
 function draw() {
     background(100);
     createCircleCanvas(600,600, 30, a);
-
-    //RandomCircles(600, 600, 1000);
-    //delay(1000);
-
     if (bool == false)   
         a -= 0.2;
     else
         a += 0.2;
-
     if (a > 60)
         bool = false;
     if (a < 30)
         bool = true;
-
-   
-
-    //ellipse(mouseX, mouseY, 80, 80);*/
 }
